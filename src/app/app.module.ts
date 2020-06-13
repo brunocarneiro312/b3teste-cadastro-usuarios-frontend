9878b80e75d10f03cd682ec70ec67ad7b3a2f297
Bruno Carneiro <bruno.carneiro312@gmail.com>
2020-06-13 19:51:53 -0300

    adicionando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ListagemComponent } from './listagem/listagem.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'listagem', component: ListagemComponent},
+  {path: 'cadastro', component: CadastroComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastroComponent,
-    ListagemComponent
+    ListagemComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
